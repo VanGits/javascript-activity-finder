18605214816aed19df2645003725445af59c5045
Van Celdran <celdranvan@gmail.com>
2022-11-08 18:19:50 -0800

    added darkMode features

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // DOM Manipulation
-
+let mainPage = document.querySelector(".main-page");
 let div = document.getElementById("main-div");
 let responseDiv = document.getElementById("main-responses");
 let circleWrapper = document.createElement("div");
 let firstResponse = document.querySelector(".response-1");
 circleWrapper.classList.add("circleWrapper");
-
 let circleArr = [1, 2, 3, 4, 5];
 
-console.log(circleArr);
+
 
 circleArr.map((circle) => {
   let circleNumber = 0;
@@ -29,7 +28,7 @@ circleArr.map((circle) => {
       div.style.display = "none";
     } else {
       div.style.display = "none";
-      handleAcitivity();
+      handleActivity(circle);
     }
   });
 });
@@ -48,6 +47,31 @@ function testFetch() {
     .then((data) => console.log(data));
 }
 
-function handleAcitivity() {
+function handleActivity(circle) {
   console.log("n");
+}
+
+let halfCircle = document.querySelector(".fa-circle-half-stroke");
+
+halfCircle.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode(e) {
+  e.preventDefault();
+  document.body.classList.add("body-darkMode")
+  document.querySelector(".main-h2").classList.add("darkMode")
+  
+
+  if (halfCircle.classList.contains("lightMode")){
+    halfCircle.classList.replace("lightMode", "darkMode")
+   
+
+  } else {
+    halfCircle.classList.replace("darkMode", "lightMode")
+    document.body.classList.remove("body-darkMode")
+    document.querySelector(".main-h2").classList.remove("darkMode")
+    
+  }
+
+  
+  
 }
